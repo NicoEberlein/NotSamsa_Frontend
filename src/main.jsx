701c7398987f24e1ab38c1135017f19b24a8936b
NicoEberlein <nico@eberln.de>
2025-06-02 16:38:26 +0500

    feat: lazy loading of components

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,20 @@
-import { StrictMode } from 'react'
+import {lazy, StrictMode} from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import 'antd/dist/reset.css';
 import '@ant-design/v5-patch-for-react-19';
 import App from './App.jsx'
 import {createBrowserRouter, RouterProvider} from "react-router-dom";
-import LoginPage from "./components/login.jsx";
 import {QueryClient, QueryClientProvider} from "@tanstack/react-query";
-import RegisterPage from "./components/register.jsx";
 import {AuthProvider} from "./contexts/AuthContext.jsx";
 import {ProtectedRoute} from "./components/ProtectedRoute.jsx";
-import CollectionsView from "./components/CollectionsView.jsx";
-import CollectionDetailView from "./components/CollectionDetailView.jsx";
+const LazyApp = lazy(() => import('./App.jsx'));
+const LazyLoginPage = lazy(() => import('./components/login.jsx'));
+const LazyRegisterPage = lazy(() => import('./components/register.jsx'));
+const LazyCollectionsView = lazy(() => import('./components/CollectionsView.jsx'));
+const LazyCollectionDetailView = lazy(() => import('./components/CollectionDetailView.jsx'));
+import {ConfigProvider, theme} from "antd";
+import {ThemeProvider, useTheme} from "./contexts/ThemeContext.jsx";
 
 const queryClient = new QueryClient();
 
@@ -21,25 +24,25 @@ const router = createBrowserRouter([
         children: [
             {
                 path: "/",
-                element: <App/>
+                element: <LazyApp/>
             },
             {
                 path: "/collections",
-                element: <CollectionsView/>
+                element: <LazyCollectionsView/>
             },
             {
                 path: "/collections/:collectionId",
-                element: <CollectionDetailView/>
+                element: <LazyCollectionDetailView/>
             }
         ]
     },
     {
         path: "/login",
-        element: <LoginPage/>,
+        element: <LazyLoginPage/>,
     },
     {
         path: "/register",
-        element: <RegisterPage/>
+        element: <LazyRegisterPage/>
     }
 ])
 
